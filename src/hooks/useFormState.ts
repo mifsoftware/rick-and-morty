b5cd62b8fd9b5ae610d3f4be78851f1cff7f84cb
Меликов Иван <mifsoftware1861@gmail.com>
2023-01-20 12:06:19 +0300

    comments to hooks

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 
+// Hook for controlled input,
+// you only need to destructure the returned object
+// like <input type="text" {...inputState} />
 const useFormState = (inititalValue: any) => {
 	const [value, setValue] = useState(inititalValue);
 	const handleChange = (e: any) => {
